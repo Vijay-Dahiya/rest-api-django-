46627a7ec68c76f98733c955c3cddc2cd285763d
Vijay-Dahiya <vijaydahiyano.1@gmail.com>
2023-02-02 13:25:22 +0530

    Done

--- companyapi/api/admin.py
+++ companyapi/api/admin.py
@@ -12,3 +12,16 @@ class EmployeeAdmin(admin.ModelAdmin) :
 
 admin.site.register(Company,CompanyAdmin)
 admin.site.register(Employee,EmployeeAdmin) 
+
+
+## json.dumps(python_data) it will conver python data to json data ; 
+# json.loads(json_data)  vice versa of dumps
+# serializeres work -> 
+# 1. -> will convert complex data types (such as querryset or models ) into native python datatypes that can easily convertable into json or html etc. 
+# 2. -> responsible for deserialization also convert parsed(serialized data) into complex data.
+
+####################
+
+# Serializer class 
+ # similar to Django form and model form class
+
